409de12f8a30b4a31c8561b2c942561d410fb885
Matt Solomon <matt@mattsolomon.dev>
2021-04-08 18:10:25 -0700

    Add docs and script for Create Protection Market

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Cozy Developer Guides
 
-This repository contains various guides to show you how to interact with the Cozy Protocol. These guides assume knowledge of [TypeScript](https://www.typescriptlang.org/), [ethers.js](https://docs.ethers.io/v5/single-page/), and [Solidity](https://docs.soliditylang.org/en/v0.8.3/), as well as familiarity with the Cozy Protocol. [Hardhat](https://hardhat.org/) is used as well, but you shouldn't need to be too familiar with Hardhat to use this repo.
+This repository contains various guides to show you how to interact with the Cozy Protocol. These guides assume knowledge of [TypeScript](https://www.typescriptlang.org/), [ethers.js](https://docs.ethers.io/v5/single-page/), and [Solidity](https://docs.soliditylang.org/en/v0.8.3/), as well as familiarity with the [Cozy Protocol](https://app.gitbook.com/@cozy-finance-1/s/cozy-docs/for-developers/technical-overview). [Hardhat](https://hardhat.org/) is used as well, but you shouldn't need to be too familiar with Hardhat to use this repo.
 
 ## Getting Started
 
@@ -9,6 +9,8 @@ All scripts in this repository are run against a forked Rinkeby, so you can have
 First, run `yarn` at the project root to install all dependencies.
 
 Next, copy the `.env.example` file, rename it to `.env`, and set the variables accordingly. The `RPC_URL` shows an Infura endpoint as the default, but you are free to use any node provider of your choice. The private key will be used to generate the primary account used in scripts, so you can either use a dummy private key and use the power of a forked network to fund it, or you can use a private key corresponding to an account that's already funded.
+
+Let's also discuss one important aspect of Cozy: TODO discuss failure error codes
 
 ## Usage
 
@@ -21,8 +23,10 @@ A script may be run with the command `yarn ts-node guides/<script-name.ts>`. Add
 
 - Some guides may only have one of the two files. Guides with both can be recognized as the `*.md` and `*.ts` files will have the same name. All TypeScript files will be heavily commented to aid understanding and readability, and TypeScript is used throughout so there's no ambiguity around what a variable or parameter is.
 - Some guides require contracts. Any required contracts live in the `contracts` folder, and you can compile the contracts in that folder with `yarn build`.
+- Contract addresses used to test against live deployments on a forked network can be found in the `deployments` folder
 
 And finally, a few notes on Hardhat:
 
 - The scripts explicitly require the Hardhat Runtime Environment with `import hre from 'hardhat'`. This is optional, but is required for running the script in a standalone fashion with `yarn ts-node <script.ts>`. When running the script with `yarn hardhat run <script>` this explicit import is unnecessary. We default to the explicit, `ts-node` approach so there's less hardhat magic and improved readability and portability. Similarly, this is why some scripts call `await hre.run('compile')`&mdash;this compiles our contracts, and would otherwise be done automatically when running with `yarn hardhat run <script>`
 - Some scripts deploy contracts by using `hre.ethers.getContractFactory()` to get the Contract Factory instance. If you want to do this without Hardhat, use the regular ethers [Contract Factory](https://docs.ethers.io/v5/single-page/#/v5/api/contract/contract-factory/) approach. Deploying contracts is not the focus of these guides, so it uses the Hardhat approach for convenience and brevity.
+- **TODO** hre.ethers
--- guides/create-protection-market.ts
+++ guides/create-protection-market.ts
@@ -1,26 +1,62 @@
 import hre from 'hardhat';
 import '@nomiclabs/hardhat-ethers';
-
 import { Contract, ContractFactory } from 'ethers';
+import { getChainId, getContractAddress, logSuccess, logFailure, findLog } from '../utils/utils';
+import comptrollerAbi from '../abi/Comptroller.json';
 
+// STEP 0: GENERIC SETUP
+// if (!process.env.PRIVATE_KEY) throw new Error('Please set your PRIVATE_KEY in a .env file');
+const provider = hre.ethers.provider;
+const signer = new hre.ethers.Wallet(process.env.PRIVATE_KEY as string, hre.ethers.provider);
+const chainId = getChainId(hre);
+const { AddressZero } = hre.ethers.constants;
+
+// STEP 1: TRIGGER CONTRACT SETUP
+// Define required constructor parameters
+const name = 'Mock Trigger'; // trigger name
+const symbol = 'MOCK'; // trigger symbol
+const description = 'A mock trigger that anyone can toggle'; // trigger description
+const platformIds = [3]; // array of platform IDs that this trigger protects
+const recipient = '0x1234567890AbcdEF1234567890aBcdef12345678'; // address of subsidy recipient
+
+// STEP 2: TRIGGER CONTRACT DEVELOPMENT
+// For this step, see the TriggerInterface and MockTrigger contracts and read the corresponding markdown file
+
+// STEP 3: PROTECTION MARKET DEPLOYMENT
 async function main(): Promise<void> {
+  // Compile contracts to make sure we're using the latest version of the trigger contracts
   await hre.run('compile');
 
-  // Define constructor parameters
-  const name = 'Mock Trigger'; // trigger name
-  const symbol = 'MOCK'; // trigger symbol
-  const description = 'A mock trigger that anyone can toggle'; // trigger description
-  const platformIds = [3]; // array of platform IDs that this trigger protects
-  const recipient = '0x60A5dcB2fC804874883b797f37CbF1b0582ac2dD'; // address of subsidy recipient
+  // Get instance of the Trigger ContractFactory with our signer attached
+  const MockTriggerFactory: ContractFactory = await hre.ethers.getContractFactory('MockTrigger', signer);
 
-  // We get the contract to deploy, deploy it, and print the address
-  const MockTrigger: ContractFactory = await hre.ethers.getContractFactory('MockTrigger');
-  const multicall: Contract = await MockTrigger.deploy(name, symbol, description, platformIds, recipient);
-  await multicall.deployed();
+  // Deploy the trigger contract
+  const trigger: Contract = await MockTriggerFactory.deploy(name, symbol, description, platformIds, recipient);
+  await trigger.deployed();
+  logSuccess(`MockTrigger deployed to ${trigger.address}`);
 
-  console.log('MockTrigger deployed to: ', multicall.address);
+  // Let's choose DAI as the underlying, so first we need to check if there's a DAI Money Market
+  const daiAddress = getContractAddress('DAI', chainId);
+  const comptrollerAddress = getContractAddress('comptroller', chainId);
+  const comptroller = new Contract(comptrollerAddress, comptrollerAbi, signer); // connect signer for sending transactions
+  const daiMoneyMarketAddress = await comptroller.getCToken(daiAddress, AddressZero);
 
-  // TODO more stuff
+  if (daiMoneyMarketAddress === AddressZero) {
+    logFailure('No DAI Money Market exists, exiting script');
+    return;
+  }
+  logSuccess(`Safe to continue: Found DAI Money Market at ${daiMoneyMarketAddress}`);
+
+  // If we're here, a DAI Money Market exists, so it's safe to create our new Protection Market. If we tried
+  // to create a new Protection Market before a DAI Money Market existed, our transaction would revert
+  const tx = await comptroller.deployProtectionMarket(daiAddress, trigger.address);
+
+  // This should emit a ProtectionMarketListed event on success, so let's check for that event. If not found, this
+  // method will throw and print the Failure error codes which can be looked up in ErrorReporter.sol
+  const { log, receipt } = await findLog(tx, comptroller, 'ProtectionMarketListed', provider);
+  logSuccess(`Success! Protection Market deployed to ${log?.args.cToken} in transaction ${receipt.transactionHash}`);
+
+  // Done! You have successfully deployed your protection market
 }
 
 // We recommend this pattern to be able to use async/await everywhere and properly handle errors.
