c0dfa26d98102d5ad1389a7ceeec2523c2ba1411
Matt Solomon <matt@mattsolomon.dev>
2021-06-18 07:04:22 -0700

    Cleanup utils and README

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,11 +1,13 @@
-import ethers from 'ethers';
+import { Contract } from '@ethersproject/contracts';
+import { JsonRpcProvider, TransactionResponse } from '@ethersproject/providers';
+import { parseUnits } from '@ethersproject/units';
 import chalk from 'chalk';
-import mainnetDeployAddresses from '../deployments/mainnet.json';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import mainnetDeployAddresses from '../deployments/mainnet.json';
 
 // Rename contract names from the verbose deploy names to something more concise
 const mainnetAddresses = {
-  ETH: mainnetDeployAddresses.ETH,
+  ETH: mainnetDeployAddresses.ETH, // placeholder ETH address
   DAI: mainnetDeployAddresses.DAI,
   USDC: mainnetDeployAddresses.USDC,
   WBTC: mainnetDeployAddresses.WBTC,
@@ -19,6 +21,7 @@ const mainnetAddresses = {
   CozyUSDC: mainnetDeployAddresses['CErc20Immutable:Money Market:Cozy USD Coin'],
   CozyWBTC: mainnetDeployAddresses['CErc20Immutable:Money Market:Cozy Wrapped BTC'],
   Maximillion: mainnetDeployAddresses['Maximillion:Maximillion'],
+  YearnProtectionMarket: '0x37e45483B5242c2b3f35ed153dDAf7CE45C1C7B2', // sample protection market deployed on mainnet
 };
 
 // Mapping of chainId to contract addresses
@@ -30,59 +33,79 @@ type ContractNames = keyof typeof mainnetAddresses;
 export const logSuccess = (msg: string) => console.log(`${chalk.green('\u2713')} ${msg}`); // \u2713 = check symbol
 export const logFailure = (msg: string) => console.log(`${chalk.red('\u2717')} ${msg}`); // \u2717 = x symbol
 
-// Gets a contract's address by it's name and chainId
+/**
+ * @notice Gets a contract's address by it's name and chainId
+ * @param name Contract name, must be a valid key from `mainnetAddresses`
+ * @param chainId Chain ID to get contract from
+ * @returns Contract address
+ */
 export const getContractAddress = (name: string, chainId: number) => {
+  if (!Object.keys(address[chainId as ChainId]).includes(name)) {
+    throw new Error(`Contract named '${name}' not found for chainId ${chainId}`);
+  }
   return address[chainId as ChainId][name as ContractNames];
 };
 
-// Gets the chainId from the hardhat configuration (normally you could get this from ethers.provider.network.chainId)
+/**
+ * @notice Gets the chainId from the hardhat configuration
+ * @dev Normally you could get this from `(await ethers.provider.getNetwork()).chainId`, but because these scripts run
+ * against a local, forked network, the chain ID is Hardhat's default value of 1337. The helper methods in this file
+ * rely on the chain ID to properly fetch contract addresses, so we define this `getChainId()` method to override the
+ * chain ID based on which network we've forked locally against
+ * @param hre Hardhat runtime environment
+ */
 export const getChainId = (hre: HardhatRuntimeEnvironment) => {
-  const defaultChainId = 1; // default to mainnet
   const forkUrl = hre.config.networks.hardhat.forking?.url;
-  if (!forkUrl) return defaultChainId;
+  if (!forkUrl) throw new Error('Fork URL not configured');
   if (forkUrl.includes('mainnet')) return 1;
   if (forkUrl.includes('ropsten')) return 3;
   if (forkUrl.includes('rinkeby')) return 4;
   if (forkUrl.includes('goerli')) return 5;
   if (forkUrl.includes('kovan')) return 42;
-  return defaultChainId;
-};
-
-// When operating on a forked network, our account needs to get tokens somehow. When forking mainnet, we can either use
-// private key corresponding to an account that already has the needed tokens, or we can use Hardhat's
-// `hardhat_impersonateAccount` RPC method to transfer ourselves tokens from any account that has them. On Rinkeby,
-// anyone can mint the required tokens, so we simply mint them
-export const supplyTokensTo = async (
-  symbol: string,
-  amount: string,
-  to: string,
-  hre: HardhatRuntimeEnvironment,
-  signer: ethers.Wallet
-) => {
-  // symbol is our token symbol, and amount is the human-readable amount. Currently this method only supports Rinkeby
-  const chainId = getChainId(hre);
-  if (chainId !== 4) throw new Error('supplyTokensTo: Unsupported network');
-  const tokenAddress = getContractAddress(symbol, chainId);
-  const mintAbi = ['function mint(address,uint256) returns (bool)', 'function decimals() view returns (uint8)'];
-  const token = new hre.ethers.Contract(tokenAddress, mintAbi, signer);
-  const decimals = await token.decimals();
-  await token.mint(to, hre.ethers.utils.parseUnits(amount, decimals));
+  throw new Error('Unsupported network');
 };
 
 /**
- * @notice: Returns true if event named `logName` was emitted by `contract`
- * in the provided `tx
- * @param tx: ethers contract call response, of type ContractTransaction
- * @param contract: Instance of an ethers Contract
- * @param logName: Name of the log to look for
- * @param provider: Provider to use
- * @returns receipt if Log was found, throws and prints error codes if not
+ * @notice Fund an account on a forked network with tokens
+ * @dev Currently only configured to work on mainnet
+ * @dev When operating on a forked network, our accounts need to get tokens somehow. When forking mainnet, we can
+ * either use an initial set of accounts corresponding to accounts that already have the needed tokens, or we can use
+ * Hardhat's `hardhat_impersonateAccount` RPC method to transfer ourselves tokens from any account that has them.
+ * @param tokenAddress Address of token
+ * @param amount Amount to send in human-readable form
+ * @param to Account to send the tokens to
+ * @param hre HardhatRuntimeEnvironment, used for getting chainID
+ */
+export const fundAccount = async (tokenAddress: string, amount: string, to: string, hre: HardhatRuntimeEnvironment) => {
+  const chainId = getChainId(hre);
+  if (chainId !== 1) throw new Error('Unsupported network');
+  const tokenAbi = ['function transfer(address,uint256) returns (bool)', 'function decimals() view returns (uint8)'];
+
+  // We impersonate the binance exchange account on mainnet as a source of tokens
+  const exchange = '0x28C6c06298d514Db089934071355E5743bf21d60';
+  await hre.network.provider.request({ method: 'hardhat_impersonateAccount', params: [exchange] });
+  const signer = await hre.ethers.provider.getSigner(exchange);
+
+  // Transfer tokens from the exchange to the wallet
+  const token = new Contract(tokenAddress, tokenAbi, signer);
+  const decimals = await token.decimals();
+  await token.transfer(to, parseUnits(amount, decimals));
+  await hre.network.provider.request({ method: 'hardhat_stopImpersonatingAccount', params: [exchange] });
+};
+
+/**
+ * @notice Returns true if event named `event` was emitted by `contract` in the provided `tx
+ * @param tx ethers contract call response, of type ContractTransaction
+ * @param contract Instance of an ethers Contract
+ * @param event Name of the log to look for
+ * @param provider Provider instance
+ * @returns receipt if log was found, throws and prints error codes if not
  */
 export const findLog = async (
-  tx: ethers.providers.TransactionResponse,
-  contract: ethers.Contract,
-  logName: string,
-  provider: ethers.providers.JsonRpcProvider
+  tx: TransactionResponse,
+  contract: Contract,
+  event: string,
+  provider: JsonRpcProvider
 ) => {
   // Wait for the transaction to be mined, then get the transaction receipt
   await tx.wait();
@@ -92,7 +115,7 @@ export const findLog = async (
   const logs = receipt.logs.map(parseLog(contract));
 
   // For each log in logs, find the first one with a name equal to our target `logName`
-  const log = logs.filter((log) => log?.name === logName)[0];
+  const log = logs.filter((log) => log?.name === event)[0];
 
   // Found, return the parsed log information and the receipt
   if (log) return { log, receipt };
@@ -106,14 +129,13 @@ export const findLog = async (
 };
 
 /**
- * @notice Wrapper around ethers' parseLog that returns undefined instead of
- * throwing an error (an error is throw if we try parsing a log with the wrong
- * interface)
- * @param contract: Instance of an ethers Contract
- * @param log: A single `Log` from the tx receipt's logs array
+ * @notice Wrapper around ethers' parseLog that returns undefined instead of throwing an error (by default an error is
+ * thrown if you try parsing a log with the wrong interface)
+ * @param contract Instance of an ethers Contract
+ * @param log A single `Log` from the tx receipt's logs array
  * @returns The parsed log, or undefined if it could not be parsed
  */
-const parseLog = (contract: ethers.Contract) => (log: { topics: Array<string>; data: string }) => {
+const parseLog = (contract: Contract) => (log: { topics: Array<string>; data: string }) => {
   try {
     return contract.interface.parseLog(log);
   } catch (err) {
