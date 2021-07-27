2a3c59dc809a70885359247d20af830849c8148e
Lisa Gunn <lisa.s.gunn@gmail.com>
2021-07-27 12:31:03 -0700

    A couple of typos, a couple of text suggestions

--- contracts/interfaces/ITrigger.sol
+++ contracts/interfaces/ITrigger.sol
@@ -2,13 +2,13 @@ pragma solidity ^0.8.5;
 
 /**
  * @notice Abstract contract for creating or interacting with a Trigger contract
- * @dev All trigger contracts created must inerit from this contract and conform to this interface
+ * @dev All trigger contracts created must inherit from this contract and conform to this interface
  */
 abstract contract ITrigger {
-  /// @notice Trigger name, analgous to an ERC-20 token's name
+  /// @notice Trigger name, equivalent to an ERC-20 token's name
   string public name;
 
-  /// @notice Trigger symbol, analgous to an ERC-20 token's symbol
+  /// @notice Trigger symbol, equivalent to an ERC-20 token's symbol
   string public symbol;
 
   /// @notice Trigger description
@@ -17,7 +17,7 @@ abstract contract ITrigger {
   /// @notice Array of IDs of platforms covered by this trigger
   uint256[] public platformIds;
 
-  /// @notice Returns address of recipient who receives subsidies for creating a protection market using this trigger
+  /// @notice Returns the address of the recipient who receives subsidies for creating a protection market using this trigger
   address public immutable recipient;
 
   /// @notice Returns true if trigger condition has been met
@@ -41,7 +41,7 @@ abstract contract ITrigger {
   }
 
   /**
-   * @notice Returns array of IDs, where each ID corresponds to a platform covered by this trigger
+   * @notice Returns an array of IDs, where each ID corresponds to a platform covered by this trigger
    * @dev See documentation for mapping of ID numbers to platforms
    */
   function getPlatformIds() external view returns (uint256[] memory) {
@@ -49,23 +49,23 @@ abstract contract ITrigger {
   }
 
   /**
-   * @dev Executes trigger-specific logic to check if market has been triggered
-   * @return True if trigger condition occured, false otherwise
+   * @dev Executes trigger-specific logic to check if a protection market has been triggered
+   * @return True if the trigger condition occurred, false otherwise
    */
   function checkTriggerCondition() internal virtual returns (bool);
 
   /**
-   * @notice Checks trigger condition, sets isTriggered flag to true if condition is met, and returns the trigger status
-   * @return True if trigger condition occured, false otherwise
+   * @notice Checks the trigger condition, sets isTriggered flag to true if condition is met, and returns the trigger status
+   * @return True if trigger condition occurred, false otherwise
    */
   function checkAndToggleTrigger() external returns (bool) {
-    // Return true if trigger already toggled
+    // Return true if the trigger condition has already been toggled
     if (isTriggered) return true;
 
-    // Return false if market has not been triggered
+    // Return false if the protection market has not been triggered
     if (!checkTriggerCondition()) return false;
 
-    // Otherwise, market has been triggered
+    // Return true if the protection market market has been triggered
     emit TriggerActivated();
     isTriggered = true;
     return isTriggered;
